269d149fe32c09070389ae56e98f6b2f2c5ee2f0
Alispahic Hamdo <hamdoalispahic@outlook.de>
2022-11-28 15:24:56 +0100

    wos

---
+++ src/Launcher.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Launcher {
+
+    /**
+     * @Todo
+     * Click the green "play" - icon to run the applictaion.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        main(args);
+    }
+
+}
